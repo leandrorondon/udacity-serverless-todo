303f07356fd9f247b4477bc393ab2c32f50d91b3
Leandro Rondon <lerondon@gmail.com>
2021-06-19 18:08:20 -0300

    add GenerateUploadUrl code and configuration in serverless.yml

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,35 @@
 import 'source-map-support/register'
-
+import * as AWS  from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+
+
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
+
+const bucketName = process.env.S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
+  const url = getUploadUrl(todoId)
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      url
+    })
+  }
+}
+
+function getUploadUrl(itemId: string): string {
+  console.log("expiration:", urlExpiration)
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: itemId,
+    Expires: parseInt(urlExpiration)
+  })
 }
